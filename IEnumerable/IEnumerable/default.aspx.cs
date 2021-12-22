32bd2f89ec4a94ae6993ba7257dbe39511a2e08e
Darshan Makhecha <65150757+darshan-makhecha@users.noreply.github.com>
2021-12-22 16:45:35 +0530

    c# program

---
+++ IEnumerable/IEnumerable/default.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections;
+
+namespace IEnumerable
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //display karva 
+            foreach (var cust in GetAllCustomer())
+            {
+                Response.Write("Name: " + cust.Name + "<br> " + "City: " + cust.City + " <br> "
+                          + "Mobile " + cust.Mobile + "<br> " + "Amount :" + cust.Amount.ToString("c") + "<br>" + "-----" + "<br>");
+            }
+        }
+        //create a class
+        public class Customer
+        {
+            private String _Name, _City;
+            private long _Mobile;
+            private double _Amount;
+
+            public String Name
+            {
+                get { return _Name; }
+                set { _Name = value; }
+            }
+            public String City
+            {
+                get { return _City; }
+                set { _City = value; }
+            }
+
+            public long Mobile
+            {
+                get { return _Mobile; }
+                set { _Mobile = value; }
+            }
+            public double Amount
+            {
+                get { return _Amount; }
+                set { _Amount = value; }
+            }
+        }
+        //data 
+        Customer[] customers = new Customer[]
+        {
+
+            new Customer {Name="Vithal Wadje",City="Mumbai",Mobile=9999999,Amount=89.45 },
+            new Customer { Name = "Sudhir Wadje", City = "Latur", Mobile = 888888888, Amount =426.00 },
+            new Customer { Name = "Anil", City = "Delhi", Mobile = 777777777777, Amount = 5896.20 }
+        };
+        //with the use of IEnumerable
+        public IEnumerable<Customer> GetAllCustomer()
+        {
+            return customers;
+        }
+    }
+}
+    
---
+++ array/array/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] staticIntArray = new int[3];  //declare an array
+            staticIntArray[0] = 1;              // define value
+            staticIntArray[1] = 3;
+            staticIntArray[2] = 5;
+            // Read array items one by one
+            Console.WriteLine(staticIntArray[0]);
+            Console.WriteLine(staticIntArray[1]);
+            Console.WriteLine(staticIntArray[2]);
+
+
+            Console.WriteLine("====================================");
+            // Initialize a dynamic array items during declaration
+            string[] strArray = new string[] {
+                 "Mahesh Chand",
+                "Mike Gold",
+                "Raj Beniwal",
+                "Praveen Kumar",
+                "Dinesh Beniwal"
+            };
+            // Read array items using foreach loop
+            foreach (string str in strArray)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
