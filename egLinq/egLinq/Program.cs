396dbe201885e5d9d9dc697a12a1934038af1978
Darshan Makhecha <65150757+darshan-makhecha@users.noreply.github.com>
2021-12-24 18:07:37 +0530

    c sharp program

---
+++ egLinq/egLinq/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace egLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integerList = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+            //LINQ Query using Query Syntax  
+            var QuerySyntax = from number  in integerList where obj > 5 select obj;
+            //Execution  
+            foreach (var item inQuerySyntax)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadKey();
+        }
+
+    }
+
+    }
+}
---
+++ linqEg1/linqEg1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace linqEg1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            // Data source
+            string[] names = { "Bill", "Steve", "James", "Mohan" };
+
+            // LINQ Query 
+            var myLinqQuery = from name in names
+                              where name.Contains('a')
+                              select name;
+
+            // Query execution
+            foreach (var name in myLinqQuery)
+                Console.Write(name + " ");
+        }
+    }
+}
