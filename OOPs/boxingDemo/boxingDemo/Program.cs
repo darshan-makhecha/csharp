c3dbf53e3bbf7f39609abacb59602e2c77d4d2ae
Darshan Makhecha <65150757+darshan-makhecha@users.noreply.github.com>
2022-01-28 17:28:39 +0530

    new program

---
+++ OOPs/boxingDemo/boxingDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace boxingDemo
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int x;
+            object obj;
+            x = 10;
+            obj = x; // box int x into an object 
+            //int Y = (int)obj; //unbox obj to int by casting
+            Console.WriteLine(obj);
+        }
+    }
+}
---
+++ OOPs/pointer/pointer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace pointer
+{
+    class MyClass
+    {
+        public unsafe void Method()
+        {
+            int x = 10;
+            int y = 20;
+            int* ptr1 = &x;
+            int* ptr2 = &y;
+            Console.WriteLine("the address is: ");
+            Console.WriteLine((int)ptr1);// Displays the memory address  
+            Console.WriteLine((int)ptr2);
+            Console.WriteLine("the value is  :");
+            Console.WriteLine(*ptr1);
+            Console.WriteLine(*ptr2);
+        }
+    }
+    class MyClient
+    {
+        public static void Main()
+        {
+            MyClass mc = new MyClass();
+            mc.Method();
+        }
+    }
+}
+
